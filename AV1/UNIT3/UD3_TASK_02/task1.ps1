89cd0380283c5602f16037434579a577f08f9f6b
Pau <42194.5a5@gmail.com>
2022-11-04 13:32:13 +0100

    UD3 TASK2 SAVES

---
+++ AV1/UNIT3/UD3_TASK_02/task1.ps1
@@ -0,0 +1,22 @@
+#author pau
+[int]$var = $args.Count
+
+if ( $var -eq 1 ) {
+    Write-Host "You have chosen"
+
+}elseif ( $var -eq 2 ) {
+    Write-Host "You have chosen"
+
+}elseif ( $var -eq 3 ){ 
+    Write-Host "You have chosen"
+
+}elseif ( $var -eq 4 ){ 
+    Write-Host "You have chosen"
+
+}elseif ( $var -eq 5 ){ 
+    Write-Host "You have chosen"
+
+}else {
+    Write-Host "That is not inside user stardart [1-5]"
+}
+
