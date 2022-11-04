041a2443969e117535e1f7ac45694dab81205c4e
Pau <42194.5a5@gmail.com>
2022-11-04 13:38:42 +0100

    UD3 TASK2 SAVES2

--- AV1/UNIT3/UD3_TASK_02/task1.ps1
+++ AV1/UNIT3/UD3_TASK_02/task1.ps1
@@ -2,21 +2,22 @@
 [int]$var = $args.Count
 
 if ( $var -eq 1 ) {
-    Write-Host "You have chosen"
-
+    Write-Host "YOU HAVE CHOSEN %APPDATA%"
+    Write-Host "IT SHOWS === C:\Users\USUARIO\AppData\Roaming"
 }elseif ( $var -eq 2 ) {
-    Write-Host "You have chosen"
-
+    Write-Host "YOU HAVE CHOSEN %TEMP%"
+    Write-Host "IT SHOWS === C:\Users\USUARIO\AppData\Local\Temp"
 }elseif ( $var -eq 3 ){ 
-    Write-Host "You have chosen"
-
+    Write-Host "YOU HAVE CHOSEN %COMSPEC%"
+    Write-Host "IT SHOWS === C: Windows System32 cmd.exe"
 }elseif ( $var -eq 4 ){ 
-    Write-Host "You have chosen"
-
+    Write-Host "YOU HAVE CHOSEN %USERDOMAIN%"
+    Write-Host "DOMAIN OF THE ACTUAL USER"
 }elseif ( $var -eq 5 ){ 
-    Write-Host "You have chosen"
-
+    Write-Host "YOU HAVE CHOSEN %PSModulePath%"
+    Write-Host "IT SHOWS === %SystemRoot% system32 WindowsPowerShell v1.0 Modules"
 }else {
-    Write-Host "That is not inside user stardart [1-5]"
+    Write-Host "WRONG INPUT **** [1-5]"
 }
 
+Write-Host "IN ALL CASES WE CAN EXECUTE WITH 'WIN+R' OR IN TERMINAL"
