9c852f08e05829a275e33056349caccb8c1e30e5
Pau <42194.5a5@gmail.com>
2022-11-07 11:30:23 +0100

    UPLOAD CHANGES IN TASK 4

--- AV1/UNIT3/UD3_TASK_04/script.sh
+++ AV1/UNIT3/UD3_TASK_04/script.sh
@@ -2,27 +2,51 @@
 #PAU <3
 rc=0
 folder=/home/$1/Workspace
+folderarray=(config bin source rsrc)
+cr=0
 
-if [ $# -eq 2 ]; then 
+if [ $# -le 3 ]; then 
     id | grep -q $1 || rc=1    
     if [ $rc -eq 0 ]; then
         echo "**[CORRECT] -- Valid username"
         if [ $2 = "replenish" ]; then
             if [ -d $folder ]; then
                 rm -r $folder
-                echo "aaaaaa"
+                echo "**[WARNING] -- Folder already exist.. updating*****"
+                mkdir $folder
             else
                    mkdir  $folder
-                    echo "folder created"
+                    echo "[CORRECT] -- Folder created**"
             fi
-     
-
-
-
-
+            cd $folder
+            for x in "${folderarray[@]}" ; do
+                mkdir $x
+                cd $x
+                touch sample.txt
+                cd ..
+            done
 
         elif [ $2 = "test" ]; then
-            echo "test selected"
+            if [ $# -eq 3 ]; then
+                if [ -e $3 ]; then
+                    test $3 || cr=1 
+                    if [ $cr -eq 0 ]; then
+                        echo "**[CORRECT] -- The folder given does exist"                        
+                        if [ -r $3 ]; then
+                            echo "**[CORRECT] -- The folder is readable"
+                        else
+                            echo "**[WARNING] -- The folder does not have read permissions"
+                        fi
+                    else
+                        echo "**[WARNING] -- The folder given does not exist"
+                    fi
+                else
+                    "**[WARNING] -- Unexpected input or it doesn-t exist"
+                fi
+            else
+                echo "**[WARNING] -- Unexpected input"
+            fi
+
         elif [ $2 = "clean" ]; then
             echo "clean selected"
         else
