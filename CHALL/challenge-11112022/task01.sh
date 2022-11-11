1e3b024f0e3f3747ad24cd1cb25e9c7d61d208ff
Pau <42194.5a5@gmail.com>
2022-11-11 18:57:50 +0100

    ADDED FILES TO CHALLENGE

--- CHALL/challenge-11112022/task01.sh
+++ CHALL/challenge-11112022/task01.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #author=pau gradoli <3
+param=$1
 path=$HOME/games-paths/friday-for-friday
 datetoday=$(date +%I%M%S%d%m%Y)
 
+
 if [ $# -eq 1 ]; then
-	case $1 in
+	case $param in
         clean)
             rm -r $path
             echo "OLD GAME-FILE ERASED [START YOUR NEW WORLD>>>>>>START]"
@@ -16,28 +18,44 @@ if [ $# -eq 1 ]; then
             echo $datetoday-start >> $path/log.txt
         ;;
         green)
-            mkdir -p $path
-            mkdir -p $path/Green-Level
-            touch $path/Green-Level/$datetoday-friday-start.tkn
-            echo $datetoday-green >> $path/log.txt    
+            if [ -d $path/Start ]; then
+                mkdir -p $path
+                mkdir -p $path/Green-Level
+                touch $path/Green-Level/$datetoday-friday-start.tkn
+                echo $datetoday-green >> $path/log.txt    
+            else
+                echo "MISSING STAGE"            
+            fi
         ;;
         yellow)
-            mkdir -p $path
-            mkdir -p $path/Yellow-Level
-            touch $path/Yellow-Level/$datetoday-friday-yellow.tkn
-            echo $datetoday-yellow >> $path/log.txt 
+            if [ -d $path/Green-Level ]; then
+                mkdir -p $path
+                mkdir -p $path/Yellow-Level
+                touch $path/Yellow-Level/$datetoday-friday-yellow.tkn
+                echo $datetoday-yellow >> $path/log.txt 
+            else
+                echo "MISSING STAGE"            
+            fi
         ;;
         red)
-            mkdir -p $path
-            mkdir -p $path/Red-Level
-            touch $path/Red-Level/$datetoday-friday-red.tkn
-            echo $datetoday-red >> $path/log.txt 
+            if [ -d $path/Yellow-Level ]; then
+                mkdir -p $path
+                mkdir -p $path/Red-Level
+                touch $path/Red-Level/$datetoday-friday-red.tkn
+                echo $datetoday-red >> $path/log.txt 
+            else
+                echo "MISSING STAGE"            
+            fi
         ;;
         pirates)
-            mkdir -p $path
-            mkdir -p $path/Pirates-Level
-            touch $path/Pirates-Level/$datetoday-friday-pirates.tkn
-            echo $datetoday-pirates >> $path/log.txt 
+            if [ -d $path/Red-Level ]; then
+                mkdir -p $path
+                mkdir -p $path/Pirates-Level
+                touch $path/Pirates-Level/$datetoday-friday-pirates.tkn
+                echo $datetoday-pirates >> $path/log.txt 
+            else
+                echo "MISSING STAGE"            
+            fi
         ;;
         skip)
             echo "[ARE YOU STUPID OR WHAT??!]"
